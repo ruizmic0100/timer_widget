6f505c117861da8ec678915892d49981847d0196
Michael <ruizmic0100@hotmail.com>
2023-04-10 18:42:16 -0500

    Init commit. Has basic functionality of using the tkinter lib.

---
+++ Timer_Widget.py
@@ -0,0 +1,40 @@
+import tkinter as tk
+from tkinter import ttk
+
+def button_func():
+    print('a button was pressed')
+
+def exericise_button_func():
+    print('hello')
+
+# create a window
+window = tk.Tk()
+window.title('Window and Widgets')
+window.geometry('800x500')
+
+# ttk label
+label = ttk.Label(master = window, text = 'This is a test')
+label.pack()
+
+#tk.text
+text = tk.Text(master = window)
+text.pack()
+
+# ttk entry
+entry = ttk.Entry(master = window)
+entry.pack()
+
+# exercise label
+exercise_label = ttk.Label(master = window, text = "my label")
+exercise_label.pack()
+
+# ttk button
+button = ttk.Button(master = window, text = 'A button', command = button_func)
+button.pack()
+
+# exercise button
+exercise_button = ttk.Button(master = window, text = 'exercise button', command = lambda: print('hello'))
+exercise_button.pack()
+
+# run
+window.mainloop()
